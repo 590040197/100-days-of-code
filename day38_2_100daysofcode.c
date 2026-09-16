47b9505f371a7c1dd6689b7d3bbed6afc44a87ed
590040197 <Abhinav.40197@stu.upes.ac.in>
2026-09-16 23:41:09 +0530

    Add files via upload

---
+++ day38_2_100daysofcode.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+    int symmetric = 1;
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+
+    return 0;
+}
